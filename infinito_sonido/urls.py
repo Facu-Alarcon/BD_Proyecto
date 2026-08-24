5ac18d4d858818e65f1f470a1e3701e997e16985
Facu-Alarcon <facualar01@gmail.com>
2026-08-24 18:22:52 -0300

    Implementa URLs y lógica para CRUD

--- infinito_sonido/urls.py
+++ infinito_sonido/urls.py
@@ -2,7 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Inicio / Landing
     path('', views.inicio, name='inicio'),
+    
+    # Módulo Equipos
     path('equipos/', views.listaEquipos, name='lista_equipos'),
     path('equipos/crear/', views.createEquipos, name='createEquipos'),
+
+    #Modulo Servicio
+
+    #Modulo Clientes
+
+    #Modulo Pagos
 ]
