444f52ee3ff24459e0db0e351b3465cda89871c7
AyelenSaez <saezayelenaraceli@gmail.com>
2026-09-09 10:55:33 -0300

    Elimine los templates y vistas de Django reemplazados por el front en React

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('infinito_sonido.api_urls')),
-    path('', include('infinito_sonido.urls')),
 ]
